56ec70acedc9a33606d744333dd14e306e3f8f52
Кирил Пенчев <texnika01@gmail.com>
2019-10-11 16:29:29 +0300

    clone on laptop

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.0.1 on 2019-10-11 08:41:23.
+ * Generated for Laravel 6.0.1 on 2019-10-11 13:10:26.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
